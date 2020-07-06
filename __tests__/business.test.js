b045aba6dc025343159564f695edde91a2bfdd8a
Kyle Hubbard <kyle.james.hubbard@gmail.com>
2020-07-06 06:50:51 -0700

    meaningful fail test for test 7

--- __tests__/business.test.js
+++ __tests__/business.test.js
@@ -5,12 +5,14 @@ describe('Person age tests', () => {
   let personVenus;
   let personMars;
   let personJupiter;
+  let elderPersonMercury;
 
   beforeEach(() => {
     personMercury = new Person(10, "Mercury");
     personVenus = new Person(10, "Venus");
-    personMars = new Person(10, "Mars")
-    personJupiter = new Person(10, "Jupiter")
+    personMars = new Person(10, "Mars");
+    personJupiter = new Person(10, "Jupiter");
+    elderPersonMercury = new Person(80, "Mercury");
   })
 
   test('should correctly save age', () => {
@@ -30,10 +32,14 @@ describe('Person age tests', () => {
   });
 
   test('should convert Earth age to Jupiter age', () => {
-    expect(personJupiter.convertAge()).toEqual(0.84)
+    expect(personJupiter.convertAge()).toEqual(0.84);
   });
 
   test('should calculate life expectancy on new planet', () => {
     expect(personMercury.calcLifeExpt()).toEqual("you'd live till about 301.61 which means you have only about 259.94 years left to live!");
+  });
+
+  test('should recognize when age exceeds life expectancy', () => {
+    expect(elderPersonMercury.calcLifeExpt()).toEqual("Considering the life expectancy on Earth is 72.38 years, that means everydays a gift for you since the life expectancy on Mercury is 301.58");
   });
 });
